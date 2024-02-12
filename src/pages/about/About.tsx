4cc89d6bf1d77fcb4140dd63cdaefa11cecfc327
Collin Rémi <sharon.code@proton.me>
2024-02-12 17:26:34 +0100

    set langue with store

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,26 +1,19 @@
+import { useSelector } from 'react-redux';
 import styles from './about.module.css';
+import { getAbout, getLangue } from '@/router/selectors';
 
 const About = () => {
+   const { content, title } = useSelector(getAbout);
+   const langue = useSelector(getLangue);
    return (
       <div className={styles.container}>
-         <h1 className={styles.title}>About</h1>
+         <h1 className={styles.title}>{title[langue]}</h1>
          <div className={styles.contentContainer}>
-            <p className={styles.paragraph}>
-               Hi, I'm Rémi, a passionate developer who loves taking on
-               challenges to make every project a reality.
-            </p>
-            <p className={styles.paragraph}>
-               With a mind brimming with imagination, I love looking for and
-               above all finding ways of turning into reality all the crazy
-               ideas that pop into my head. My time at the Beaux Arts in Geneva
-               enabled me to unleash this full potential, which I can now
-               express through lines of code.
-            </p>
-            <p className={styles.paragraph}>
-               I hope you'll enjoy the projects I've created, and don't hesitate
-               to let me know what you think, or to submit any crazy ideas that
-               will fuel my creativity!
-            </p>
+            {content[langue].map((el, index) => (
+               <p key={index} className={styles.paragraph}>
+                  {el}
+               </p>
+            ))}
          </div>
       </div>
    );
--- src/router/selectors.tsx
+++ src/router/selectors.tsx
@@ -1,9 +1,15 @@
 import { RootState } from './store';
 
+// Data
 export const getData = (state: RootState) => {
    return state?.data;
 };
 
+export const getAbout = (state: RootState) => {
+   return getData(state).data.about;
+};
+
+// Settings
 export const getTheme = (state: RootState) => {
    return state?.settings.theme;
 };
