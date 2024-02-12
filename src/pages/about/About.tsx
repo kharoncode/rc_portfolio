96593e48765ddfa725de51632d334b50584b9890
Collin Rémi <sharon.code@proton.me>
2024-02-12 14:39:18 +0100

    edit about content

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,148 +5,21 @@ const About = () => {
       <div className={styles.container}>
          <h1 className={styles.title}>About</h1>
          <div className={styles.contentContainer}>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
+            <p className={styles.paragraph}>
+               Hi, I'm Rémi, a passionate developer who loves taking on
+               challenges to make every project a reality.
             </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
+            <p className={styles.paragraph}>
+               With a mind brimming with imagination, I love looking for and
+               above all finding ways of turning into reality all the crazy
+               ideas that pop into my head. My time at the Beaux Arts in Geneva
+               enabled me to unleash this full potential, which I can now
+               express through lines of code.
             </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
-            </p>
-            <p className={styles.test}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
-               repellat cum. Voluptate, dolorem id commodi harum quo officiis
-               pariatur consequuntur quos error vitae nesciunt repudiandae
-               beatae porro aperiam quia corporis. Lorem ipsum dolor sit amet
-               consectetur adipisicing elit. Ut, repellat cum. Voluptate,
-               dolorem id commodi harum quo officiis pariatur consequuntur quos
-               error vitae nesciunt repudiandae beatae porro aperiam quia
-               corporis. Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Ut, repellat cum. Voluptate, dolorem id commodi harum quo
-               officiis pariatur consequuntur quos error vitae nesciunt
-               repudiandae beatae porro aperiam quia corporis.
+            <p className={styles.paragraph}>
+               I hope you'll enjoy the projects I've created, and don't hesitate
+               to let me know what you think, or to submit any crazy ideas that
+               will fuel my creativity!
             </p>
          </div>
       </div>
