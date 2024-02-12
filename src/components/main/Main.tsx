de6301502a7e7b2054c151c82c139ff596c9275f
Collin Rémi <sharon.code@proton.me>
2024-02-12 15:07:18 +0100

    add Main component

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import Lorem from '../lorem/Lorem';
+import styles from './main.module.css';
+import { useState } from 'react';
+
+const Main = () => {
+   const [coordinate, setCoordinate] = useState('');
+   const [isMouseHover, setIsMouseHover] = useState(false);
+   return (
+      <div className={styles.mainContainer}>
+         <div className={styles.loremContainer}>
+            <Lorem coordinate={coordinate} isMouseHover={isMouseHover} />
+         </div>
+         <main
+            onMouseMove={(e) => {
+               setCoordinate(`${e.clientX - 70}px ${e.clientY - 20}px`);
+            }}
+            onMouseEnter={() => setIsMouseHover(true)}
+            onMouseLeave={() => setIsMouseHover(false)}
+         >
+            <Outlet />
+         </main>
+      </div>
+   );
+};
+
+export default Main;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,33 +1,24 @@
 import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
-import { Outlet } from 'react-router-dom';
 import styles from './rootLayout.module.css';
-import Lorem from '@/components/lorem/Lorem';
 import { useEffect, useState } from 'react';
+import Main from '@/components/main/Main';
 
 const RootLayout = () => {
-   const [coordinate, setCoordinate] = useState('');
-   const [isMouseHover, setIsMouseHover] = useState(false);
+   const [data, setData] = useState({});
    useEffect(() => {
-      console.log('init layout');
+      const fetchData = fetch('./data/data.json')
+         .then((resulut) => resulut.json())
+         .then((data) => {
+            return data;
+         });
+      setData(fetchData);
    }, []);
+   console.log(data);
    return (
       <div className={styles.container}>
          <Header />
-         <div className={styles.mainContainer}>
-            <div className={styles.loremContainer}>
-               <Lorem coordinate={coordinate} isMouseHover={isMouseHover} />
-            </div>
-            <main
-               onMouseMove={(e) => {
-                  setCoordinate(`${e.clientX - 70}px ${e.clientY - 20}px`);
-               }}
-               onMouseEnter={() => setIsMouseHover(true)}
-               onMouseLeave={() => setIsMouseHover(false)}
-            >
-               <Outlet />
-            </main>
-         </div>
+         <Main />
          <Footer />
       </div>
    );
