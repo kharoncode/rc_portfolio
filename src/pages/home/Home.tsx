86249a239b9b03e41c9472a9c08ae6831883a409
RC-Asus <sharon.code@proton.me>
2024-02-14 23:42:29 +0100

    init home smiley

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,21 @@
 import { useSelector } from 'react-redux';
 import styles from './home.module.css';
 import { getHome, getLangue } from '@/router/selectors';
+import { useState } from 'react';
+
+const randomSmiley = [':)', 'X)', ';)', ':D', ':p', '=)', '=D'];
 
 const Home = () => {
    const { title } = useSelector(getHome);
    const langue = useSelector(getLangue);
    const content = title[langue];
+   const [temp, setTemp] = useState(':)');
+   const [time, setTime] = useState(5000);
+   console.log('ini');
+   setTimeout(() => {
+      setTemp(randomSmiley[Math.floor(Math.random() * 6)]);
+      setTime(Math.ceil(Math.random() * 2000 + 8000));
+   }, time);
    return (
       <div className={`container`}>
          <h1 className={`${styles.title} langue fade`}>
@@ -16,7 +26,8 @@ const Home = () => {
             {content[0]} <br /> <span className={styles.name}>Rémi</span>,<br />
             {content[1]}
             <br />
-            <span className={styles.name}>{content[2]}</span> {`:)`}
+            <span className={styles.name}>{content[2]}</span>{' '}
+            <span className={`smiley fade`}>{temp}</span>
          </h2>
       </div>
    );
