04df12c416f1e513cd714546a1cc1ddade01046f
Collin Rémi <sharon.code@proton.me>
2024-02-16 16:16:01 +0100

    edit home css

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,18 +9,25 @@ const Home = () => {
    const langue = useSelector(getLangue);
    const content = title[langue];
    return (
-      <div className={`container`}>
+      <div className={`container ${styles.container}`}>
          <h1 className={`${styles.title} langue fade`}>
             <span className={styles.name}>{`{`}</span> Hello World{' '}
             <span className={styles.name}>{`}`}</span>
          </h1>
-         <h2 className={`${styles.subTitle} langue fade`}>
-            {content[0]} <br /> <span className={styles.name}>Rémi</span>,<br />
-            {content[1]}
-            <br />
-            <span className={styles.name}>{content[2]}</span> <Smiley />
+         <h2 className={`${styles.content} langue fade`}>
+            <p>{content[0]}</p>{' '}
+            <p>
+               <span className={styles.name}>Rémi</span>,
+            </p>
+            <p>{content[1]}</p>
+            <p className={styles.status}>
+               <span className={styles.name}>{content[2]}</span>
+               <Smiley />
+            </p>
          </h2>
-         <Skills />
+         <div className={styles.skillsContainer}>
+            <Skills />
+         </div>
       </div>
    );
 };
