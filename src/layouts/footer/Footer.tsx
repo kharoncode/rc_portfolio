770e91bd916090a156c2348564f19227e487e50b
Collin Rémi <sharon.code@proton.me>
2024-05-16 15:12:08 +0200

    edit data.json + add new cv

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -4,15 +4,18 @@ import linkedinIcone from '@/assets/icones/linkedin.svg';
 import mail from '@/assets/icones/mail.svg';
 import Settings from '@/components/settings/settings';
 import resume from '@/assets/icones/resume.svg';
+import { useSelector } from 'react-redux';
+import { getLangue } from '@/router/selectors';
 
 const Footer = () => {
+   const langue = useSelector(getLangue);
    return (
       <footer className={styles.container}>
          <Settings />
          <div className={styles.iconesContainer}>
             <a
                className={styles.a}
-               href="./data/collinremi_cv.pdf"
+               href={`./data/collinremi_cv_${langue}.pdf`}
                target="_blank"
             >
                <div className={styles.navIconeContainer}>
