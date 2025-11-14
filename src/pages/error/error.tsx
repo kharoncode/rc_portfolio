5a87ca26930ff75523bc44c0c950795ce8073af2
Collin Remi <sharon.code@proton.me>
2025-11-14 22:51:20 +0100

    remove cdl redirection

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,22 +1,10 @@
 import { useSelector } from 'react-redux';
 import styles from './error.module.css';
 import { getError, getLangue } from '@/router/selectors';
-import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const Error = () => {
    const { title } = useSelector(getError);
    const langue = useSelector(getLangue);
-
-   const location = useLocation();
-
-   useEffect(() => {
-      const externalRoutes = ['/cdl'];
-
-      if (externalRoutes.some(route => location.pathname.startsWith(route))) {
-         window.location.href = location.pathname;
-      }
-   }, [location]);
 
    return (
       <div className={`container ${styles.container}`}>
